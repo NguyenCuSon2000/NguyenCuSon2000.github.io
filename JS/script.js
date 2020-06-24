45f247b2a29a576303cd0ae99dd466bb625a890e
son2kweb <nguyencuson28102000@gmail.com>
2020-06-24 17:30:36 +0700

    Change now

--- JS/script.js
+++ JS/script.js
@@ -23,3 +23,57 @@
             } 
             else clearTimeout(scrollAnimation);
         }
+
+
+        // PHẦN LIÊN HỆ
+
+        function KiemTra(){
+           
+            // Kiểm tra tên
+            ten = formLienHe.txtTen.value;
+            var format = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+
+            if(ten == ""){
+                alert("Tên không được trống");
+                formLienHe.ten.focus();
+                return;
+            }
+
+            if(!isNaN(ten)){
+                alert("Tên không chứa ký tự số ");
+                formLienHe.txtTen.focus();
+                return;
+            }
+            if(ten.match(format)){
+                alert("Tên không được chứa ký tự đặc biệt");
+                formLienHe.ten.focus();
+                return true;
+            }
+
+            // Kiểm tra email
+
+            var mail = document.getElementById("email").value;
+            var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            kq = filter.test(mail);
+            if(mail=="") {
+                alert("Email không được bỏ trống !");
+                document.email.focus();
+                return;
+            }
+            
+            if (!kq) {
+                alert("Hay nhap dia chi email hop le.\nExample@gmail.com");
+                document.email.focus();
+                return;
+            }
+
+            // Kiểm tra comment
+            var comment = document.getElementById("txtBinhLuan").value;
+            if(comment==""){
+                alert("Bình luận không được trống !");
+                document.txtBinhLuan.focus();
+                return;
+            }
+        }
+        
+      
