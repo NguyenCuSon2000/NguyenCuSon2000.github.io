ec66e2933b7a20b8bf79414eb2975c4d3d4e6e94
NguyenCuSon2000 <son2kcntt@gmail.com>
2020-09-11 16:45:01 +0700

    Change 5

--- JS/script.js
+++ JS/script.js
@@ -24,7 +24,26 @@
             else clearTimeout(scrollAnimation);
         }
         
-        
+        document.addEventListener("DOMContentLoaded", function(){
+            //bat su kien cuon chuot
+            var trangthai = 'duoi300';
+            //lay ve menu
+            var doituongmenu = document.querySelector('.menu');
+            window.addEventListener('scroll', function(){
+                if(window.pageYOffset > 200){
+                    if(trangthai == "duoi300"){
+                        trangthai = 'tren300';
+                        doituongmenu.classList.add('thulai');
+                    }
+                }
+                else if(window.pageYOffset <= 200){
+                    if(trangthai == 'tren300'){
+                        doituongmenu.classList.remove('thulai');
+                        trangthai = 'duoi300';
+                    }
+                }
+            })
+        }, false)
         // PHẦN LIÊN HỆ
         
         function KiemTra(){
